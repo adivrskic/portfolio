67b6a3d97fdd8e9223d661441bbcdbc107e4babe
Adi Vrskic <adivrskic123@gmail.com>
2022-02-17 15:58:32 -0500

    change all ext

--- src/data/mohawkfactoryoutletGallery.js
+++ src/data/mohawkfactoryoutletGallery.js
@@ -1,25 +1,25 @@
 export const mohawkfactoryoutletGallery = [
   {
-    src: "images/mfo1.png",
-    thumbnail: "images/mfo1.png",
+    src: "images/mfo1.PNG",
+    thumbnail: "images/mfo1.PNG",
     thumbnailWidth: 600,
     thumbnailHeight: 350,
   },
   {
-    src: "images/mfo4.png",
-    thumbnail: "images/mfo4.png",
+    src: "images/mfo4.PNG",
+    thumbnail: "images/mfo4.PNG",
     thumbnailWidth: 150,
     thumbnailHeight: 350,
   },
   {
-    src: "images/mfo3.png",
-    thumbnail: "images/mfo3.png",
+    src: "images/mfo3.PNG",
+    thumbnail: "images/mfo3.PNG",
     thumbnailWidth: 150,
     thumbnailHeight: 350,
   },
   {
-    src: "images/mfo2.png",
-    thumbnail: "images/mfo2.png",
+    src: "images/mfo2.PNG",
+    thumbnail: "images/mfo2.PNG",
     thumbnailWidth: 500,
     thumbnailHeight: 350,
   },
