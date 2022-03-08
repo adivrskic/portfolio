f6d479ccddc2cce78938fee55df062956b158e56
Adi Vrskic <adivrskic123@gmail.com>
2022-03-08 16:02:34 -0500

    add gradient to icons

--- src/components/Stat.js
+++ src/components/Stat.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import Tooltip from "./Tooltip";
 import * as Icons from "react-icons/di";
 import { IoHeart, IoHeartOutline } from "react-icons/io5";
+import { StateContext } from "../context/StateProvider";
 import "../styles/components/stat.scss";
 
 function Stat({ icon, hearts, tooltipHeader, tooltipText }) {
   const [open, setOpen] = useState(false);
+  const [{ isDarkTheme }] = React.useContext(StateContext);
 
   const handleStatClick = () => {
     setOpen(!open);
@@ -17,7 +19,15 @@ function Stat({ icon, hearts, tooltipHeader, tooltipText }) {
 
   return (
     <div className="stat" onClick={() => handleStatClick()}>
-      <IoIcon className="stat-icon" />
+      {/* Move this somewhere */}
+      <svg width="0" height="0">
+        <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="50%">
+          <stop stopColor={isDarkTheme ? "#1ddecb" : "#1793ff"} offset="0%" />
+          <stop stopColor={isDarkTheme ? "#2ebacd" : "#762ecd"} offset="100%" />
+        </linearGradient>
+      </svg>
+
+      <IoIcon className="stat-icon" style={{ fill: "url(#gradient)" }} />
       <div className="stat-heart-container">
         {[...Array(5)].map((e, i) => (
           <span key={i}>
